bea431924e2050de7cd49585a6f516eaae679721
Rohan singh <rohansingh822004@gamil.com>
2026-09-18 21:59:10 +0530

    Create connection to Database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv"
+import connectDB from "./Database/database.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.get("/" , (req , res)=>{
     res.send("Portfolio Backend is running");
