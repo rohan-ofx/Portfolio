590eb61ef161fb21db5e65f913bb60d690a58a4a
Rohan singh <rohansingh822004@gamil.com>
2026-09-26 19:39:23 +0530

    installation of jwt and implementation of access token

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asynchandler.js";
 import APiError from "../utils/ApiErrors.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken"
 
 const registerUser = asyncHandler(async(req , res)=>{
     const {username , email, password } = req.body;
@@ -40,11 +41,79 @@ if(!createduser){
 }
 
 return res.status(201).json(
-    new ApiResponse(200,
+    new ApiResponse(201,
         createduser,
         "user Registration successfull"
     )
 )
 });
 
-export default registerUser;
+const loginuser = asyncHandler(async(req, res)=>{
+    const {email , password} = req.body;
+
+    if( !email ||!password){
+        throw new APiError(
+            400,
+            "Email and password required"
+        );
+    }
+
+    //find user
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new APiError(
+            404,
+            "User does not exists"
+        );
+    }
+
+    const ispasswordvaild = await user.isPasswordCorrect(password);
+
+    if(!ispasswordvaild){
+        throw new APiError(
+            401,
+            "invaild email or Password"
+        );
+    }
+
+    const accessToken = jwt.sign({
+        _id : user._id,
+        username : user.username,
+        email: user.email,
+        role : user.role,
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn: process.env.JWT_EXPIRES_IN || "1hr",
+    }
+);
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+    if(!loggedInUser){
+        throw new APiError(
+            500,
+            "Something went wrong while logging in"
+        );
+    }
+
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                user : loggedInUser,
+                accessToken,
+            },
+            "User logged in Successfully"
+        )
+    );
+
+
+});
+
+
+export {registerUser , loginuser};
--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import registerUser from "../controllers/user.controllers.js";
+import {registerUser , loginuser} from "../controllers/user.controllers.js";
 
 const router = Router();
 
 router.post("/register", registerUser);
+router.post("/login" , loginuser);
 
 export default router;
